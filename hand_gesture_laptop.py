47a5cc69323ecf34b6d454eee7ea3b02ac2139b1
Muhammad Ainal Haque <67086317+ainal-haque@users.noreply.github.com>
2023-02-15 22:31:17 +0600

    Add files via upload

---
+++ hand_gesture_laptop.py
@@ -0,0 +1,24 @@
+import serial
+import time
+import pyautogui
+
+ArduinoSerial = serial.Serial('COM5', 9600)
+time.sleep(2)
+
+while 1:
+    incoming = str(ArduinoSerial.readline())
+    print(incoming)
+
+    if 'back' in incoming:
+        pyautogui.press('right') 
+
+    if 'Vup' in incoming:
+        pyautogui.hotkey('ctrl', 'up')
+        
+    if 'Vdn' in incoming:
+        pyautogui.hotkey('ctrl', 'down')
+
+    if 'next' in incoming:
+        pyautogui.press('left')
+
+    incoming = "";
